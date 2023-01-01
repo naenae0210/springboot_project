34f9ce5cbf86e72df839d75605eb389ffcc68d04
naenae <naegyeong0210@gmail.com>
2023-01-01 16:42:55 +0100

    test MemberService

--- src/test/java/joinweb/join/service/MemberServiceTest.java
+++ src/test/java/joinweb/join/service/MemberServiceTest.java
@@ -2,37 +2,54 @@ package joinweb.join.service;
 
 import joinweb.join.domain.Member;
 import joinweb.join.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
-class MemberServiceTest {
+public class MemberServiceTest {
 
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
 
     @Test
-    @Rollback(false)
     public void 회원등록() throws Exception {
+
         // given
         Member member = new Member();
-        member.setName("Nae");
+        member.setName("nae");
 
-        // when
+        //when
         Long result = memberService.join(member);
 
         // then
         assertEquals(member, memberRepository.findOne(result));
     }
 
-}
+    @Test(expected = IllegalStateException.class )
+    public void 중복이름검사() throws Exception {
+
+        //given
+        Member member1 = new Member();
+        member1.setName("nae");
+        Member member2 = new Member();
+        member2.setName("nae");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        // then
+        fail("예외가 발생해야함.");
+    }
+
+    }
